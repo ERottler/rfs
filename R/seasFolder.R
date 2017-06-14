6ea270e43f26a1841678fe041a69a05d46960aaa
brry <berryboessenkool@hotmail.com>
2017-06-14 14:27:56 +0200

    seasFolder: initial release

---
+++ R/seasFolder.R
@@ -0,0 +1,29 @@
+#' @title Get path to folder with seasonality analysis + data
+#' @description Get path to (files in) the folder with seasonality analysis + data
+#' @return Path(s)
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2017
+# @seealso \code{\link{help}}, \code{\link{help}}
+#' @keywords file
+# @importFrom package fun1 fun2
+#' @export
+#' @examples
+#' seasFolder()
+#' seasFolder("dummy.txt")
+#' stopifnot(length(seasFolder("dummy.txt", "dude.xyz"))==2)
+#'
+#' @param \dots Optional filename(s) that will be appended.
+#'
+seasFolder <- function(
+...
+)
+{
+path <- "C:/Users/boessenkool/Dropbox/Promotion/6_Seasonality"
+# home PC path change:
+if(!file.exists(path)) path <- gsub("C:/Users/boessenkool", "S:", path)
+# laptop linux path change:
+if(!file.exists(path)) path <- gsub("S:", "/home/berry", path)
+if(!file.exists(path)) stop("Cannot find project path. Last try:", path)
+files <- list(...)
+if(length(files)>0) path <- file.path(path, files, fsep="/")
+path
+}
