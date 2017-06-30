d1bc1db4617f7d25f328bb9feddf82fc2843caa6
brry <berryboessenkool@hotmail.com>
2017-06-30 12:18:23 +0200

    annualMax: initial release

---
+++ R/annualMax.R
@@ -0,0 +1,56 @@
+#' Annual maxima from time series
+#'
+#' Get annual maxima from time series
+#'
+#' @return named numeric vector
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2017
+#' @seealso \code{\link{qdoyCompute}}
+#' @export
+#' @examples
+#' load(seasFolder("data/dismeta.Rdata"))
+#' annual_max <- annualMax("date","Koeln", dis)
+#' head(data.frame(annual_max))
+#' plot(as.numeric(names(annual_max)), annual_max, type="l", las=1, xlab="year")
+#'
+#' @param dates    Dates in ascending order.
+#'                 Can be charater strings or \code{\link{strptime}} results,
+#'                 as accepted (and coerced) by \code{\link{as.Date}}
+#' @param values   Values to be mapped in color with \code{\link{colPoints}}
+#' @param data     Optional: data.frame with the column names as given by dates and values
+#' @param shift    Number of days to move the year-break to.
+#'                 E.g. shift=61 for German hydrological year (Nov to Oct). DEFAULT: 0
+#' @param \dots    Further arguments passed to \code{\link{colPoints}}
+#'
+annualMax <- function(
+  dates,
+  values,
+  data,
+  shift=0,
+  ...
+)
+{
+# input columns or vectors
+if(!missing(data)) # get vectors from data.frame
+  {
+    dates <- getColumn(substitute(dates),  data)
+    values<- getColumn(substitute(values), data)
+  }
+#check input
+if(length(dates)!=length(values)) stop("length of dates and values not equal (",
+                                         length(dates),", ",length(values),").")
+# convert to date
+dates <- as.Date(dates)
+
+# shift break to other month
+if(shift<0) warning("'shift' was negative (",shift,"). Absolute value now used.")
+shift <- abs(shift)
+if(shift>366) stop("'shift' is", shift, ", but should be between 0 and 366.")
+dates <- dates + shift
+
+# Annual maxima
+# axis values
+year <- as.numeric(format(dates,"%Y"))
+mymax <- function(xx) if(all(is.na(xx))) NA else max(xx, na.rm=TRUE)
+tapply(X=values, INDEX=year, FUN=mymax)
+}
+
