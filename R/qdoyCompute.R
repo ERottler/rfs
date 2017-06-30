0b41e882e31ebbbe5b8dc522b513682ed67c355b
brry <berryboessenkool@hotmail.com>
2017-06-30 14:01:07 +0200

    qdoyCompute: completely enhanced

--- R/qdoyCompute.R
+++ R/qdoyCompute.R
@@ -5,64 +5,12 @@
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2017
 #' @seealso \code{\link{qdoyPeriods}}
 #' @keywords ts chron
-#' @importFrom berryFunctions quantileMean l2df getColumn
+#' @importFrom berryFunctions l2array getColumn
 #' @importFrom pbapply pblapply
-#' @importFrom stats dnorm
-#' @importFrom extremeStat quantGPD
+#' @importFrom extremeStat distLextreme
 #' @export
 #' @examples
-#' load(seasFolder("data/dismeta.Rdata"))
-#' dd <- selectDates(1980,2010, df=dis)[,c("date","Koeln")]
-#' RPs <- c(1,2,4,30,50,100)
-#' qdoyemp3 <- qdoyCompute("date", "Koeln", data=dd, shift=117)
-#' qdoypar3 <- qdoyCompute("date", "Koeln", data=dd, shift=117, RPs=RPs, truncate=0.8)
-#' qdoyemp0 <- qdoyCompute("date", "Koeln", data=dd, shift=117, width=0)
-#' qdoypar0 <- qdoyCompute("date", "Koeln", data=dd, shift=117, RPs=RPs, width=0, truncate=0)
-#' head(qdoyemp3)
-#' head(qdoypar3)
-#'
-#' empcols <- divPal(9, gp=T); empcols[5] <- 2
-#' parcols <- divPal(7, gp=T)
-#'
-#' pdf("qdoy.pdf", height=5)
-#' plot(1:366, 1:366, ylim=c(400,11500), las=1, type="n", lwd=2, main="width=0, par")
-#' for(i in 1:6) lines(1:366, qdoypar0[,i], col=parcols[i], lwd=2)
-#' textField(200, qdoypar0[200,], colnames(qdoypar0), col=parcols)
-#' #
-#' plot(1:366, 1:366, ylim=c(400,11500), las=1, type="n", lwd=2, main="width=0, emp")
-#' for(i in 1:9) lines(1:366, qdoyemp0[,i], col=empcols[i])
-#' textField(200, qdoyemp0[200,], colnames(qdoyemp0), col=empcols)
-#' #
-#' plot(1:366, 1:366, ylim=c(400,11500), las=1, type="n", lwd=2, main="width=3, emp")
-#' for(i in 1:9) lines(1:366, qdoyemp3[,i], col=empcols[i])
-#' textField(200, qdoyemp3[200,], colnames(qdoyemp3), col=empcols)
-#' #
-#' plot(1:366, 1:366, ylim=c(400,11500), las=1, type="n", lwd=2, main="width=3, par")
-#' for(i in 1:6) lines(1:366, qdoypar3[,i], col=parcols[i], lwd=2)
-#' textField(200, qdoypar3[200,], colnames(qdoypar3), col=parcols)
-#' #
-#' plot(1:366, 1:366, ylim=c(400,11500), las=1, type="n", lwd=2, main="width=0, par")
-#' for(i in 1:6) lines(1:366, qdoypar0[,i], col=parcols[i], lwd=2)
-#' textField(200, qdoypar0[200,], colnames(qdoypar0), col=parcols)
-#' #
-#' ciBand(qdoypar0[,"RP.100"],qdoypar0[,"RP.30"],qdoypar0[,"RP.50"])
-#' lines(1:366, qdoyemp0[,"100%"], col=2)
-#' #
-#' ciBand(qdoypar0[,"RP.100"],qdoypar0[,"RP.30"],qdoypar0[,"RP.50"])
-#' ciBand(qdoypar3[,"RP.100"],qdoypar3[,"RP.30"],qdoypar3[,"RP.50"], colm=4, add=T)
-#' dev.off()
-#'
-
-#' lines(1:366, qdoypar0[,"RP.100"], lwd=2)
-#'
-
-#'
-#'
-#' sum(format(dd$date,"%m-%d")=="04-14")
-#' qdoy2 <- qdoyCompute("date", "Koeln", data=dd)
-#' head(qdoy2)
-#' qdoypar2 <- qdoyCompute("date", "Koeln", data=dd, RPs=c(1,4,10,20,30))
-#' head(qdoypar2)
+#' # to be abstracted from testing
 #'
 #' @param dates    Dates in ascending order.
 #'                 Can be charater strings or \code{\link{strptime}} results,
@@ -71,19 +19,8 @@
 #' @param data     Optional: data.frame with the column names as given by dates and values
 #' @param shift    Number of days prior to Jan 1 to move the year-break to.
 #'                 E.g. shift=61 for German hydrological year (Nov to Oct). DEFAULT: 0
-#' @param probs    Probabilities passed to \code{\link{quantileMean}} for plot=4.
-#'                 DEFAULT: c(0,25,50,75,95,99)/100
-#' @param RPs      Return periods. If given, \code{extremeStat::\link[extremeStat]{distLextreme}}
-#'                 is used to deternime return levels. DEFAULT: NA
-#' @param truncate If \code{!is.na(RPs)}, truncate is passed to distLextreme.
-#'                 If \code{width=0}, set this to zero as well. DEFAULT: 0.9
-#' @param width    Numeric: window width , used as sd in gaussian weighting.
-#'                 Support (number of values around a DOY passed to
-#'                 quantile funtion at least once) is ca 4.9*width.
-#'                 The value at doy itself is used 10 times.
-#'                 Larger values of width require more computing time.
-#'                 DEFAULT: 3
-#' @param quiet    Logical: suppress progress bar? DEFAULT: FALSE
+#' @param RPs      Return periods for \code{extremeStat::\link[extremeStat]{distLextreme}}
+#'                 DEFAULT: c(1,2,4,30,50,100)
 #' @param \dots    Further arguments, currently ignored
 #'
 qdoyCompute <- function(
@@ -91,11 +28,7 @@ dates,
 values,
 data,
 shift=0,
-probs=c(0,25,50,75,90,95,99,99.9,100)/100,
-RPs=NA,
-truncate=0.9,
-width=3,
-quiet=FALSE,
+RPs=c(1,2,5,10,20,50,100,200),
 ...
 )
 {
@@ -110,63 +43,77 @@ if(!missing(data)) # get vectors from data.frame
 if(length(dates)!=length(values)) stop("length of dates and values not equal (",
                                          length(dates),", ",length(values),").")
 dates <- as.Date(dates)
-# shift break to other month
-if(shift<0) warning("'shift' was negative (",shift,"). Absolute value now used.")
-shift <- abs(shift)
-if(shift>366) stop("'shift' is", shift, ", but should be between 0 and 366.")
+# shift break to other month:
+shift <- checkShift(shift)
 dates <- dates + shift
-doy  <- as.numeric(format(dates,"%j")) # Day of Year
-
-# empirical quantiles instead of GPD through return periods?
-empirical <- all(is.na(RPs))
-
-quantdoy1 <- function(day, emp=empirical)
-{
- if(emp) return(quantileMean(values[which(doy==day)], probs=probs, na.rm=TRUE))
- quantGPD(values[which(doy==day)], probs=1 - 1/RPs, truncate=truncate)
- #plotLextreme(dle, log=TRUE)
- #plotLfit(dle)
- #dle$returnlev[c("gpa","weighted2"),]
- #hist(values[select])
- #out <- as.numeric(dle$returnlev[1,])
- #names(out) <- colnames(dle$returnlev)
- #out
-}
-
-quantdoy2 <- function(selection, emp=empirical, doyindex)
-{
- if(emp) return(quantileMean(values[selection], probs=probs, na.rm=TRUE))
- #selection <- unique(selection)
- quantGPD(values[selection], probs=1-1/(RPs*npy), truncate=truncate)
+# Day of Year and Return Level:
+doy  <- as.numeric(format(dates,"%j"))
+doy # stiffle rstudio warning about not being used
+RL <- pblapply(1:366, function(day) distLextreme(values[doy==day], RPs=RPs, sel="gev",
+                                      quiet=TRUE, gpd=FALSE, weight=FALSE)$quant)
+# Transform into array:
+RL <- l2array(RL)
+names(dimnames(RL)) <- c("dist","rl","doy")
+dimnames(RL)[[2]] <- c(paste0("RP.", RPs), "RMSE")
+dimnames(RL)[[3]] <- 1:366
+RL
 }
 
 
-if(width<1)
-  {
-  if(!empirical) if(truncate>0) message("Note in qdoyCompute: width=0, but truncate=0.9. ",
-                         "Recommended to set to zero (Block Maxima approach).")
-  Qp <- lapply(1:366, quantdoy1)
-  }
-else
+
+# Testing ----
+if(FALSE){
+
+
+load(seasFolder("data/dismeta.Rdata"))
+dd <- selectDates(1980,2020, df=dis)[,c("date","Koeln")]
+qdoy   <- qdoyCompute("date", "Koeln", data=dis, shift=117) # 10 secs
+qdoy30 <- qdoyCompute("date", "Koeln", data=dd,  shift=117) # 5 secs
+
+rpcols <- divPal(8, gp=T)
+names(rpcols) <- paste0("RP.",c(1,2,5,10,20,50,100,200))
+
+plotqdoy <- function(dist, qd=qdoy, RPs=c(1,2,10,50,200), cols=rpcols, ylim=lim0(10900)  )
 {
-xx4 <- unique(ceiling(  (-3*width):(3*width)  ))
-w <- dnorm(xx4, sd=width)
-w <- round(w*10/max(w))
-npy <- 1#sum(w!=0)
-# computing weighted quantile around DOYs
-if(!quiet) lapply <- pbapply::pblapply
-Qp <- lapply(1:366, function(day)
-  {
-  doyindex <- which(doy==day)
-  select <- base::lapply(doyindex, function(i) rep(i+xx4, w) )
-  select <- unlist(select)
-  select <- select[select>0 & select<length(doy)]
-  # output: weighted quantile
-  quantdoy2(select, doyindex=doyindex)
-  })
+plot(1:366, 1:366, ylim=ylim, las=1, type="n", main=dist, xaxt="n", xlab="",
+     ylab="discharge  [m\U{00B3}/s]")
+seasAxis(shift=117)
+RPs <- paste0("RP.",RPs)
+for(i in RPs) lines(1:366, qd[dist,i,], col=cols[i], lwd=2)
+textField(200, qd[dist,RPs,200], RPs, col=cols[RPs])
 }
-Qp <- as.matrix(l2df(Qp))
-names(dimnames(Qp)) <- c("DOY","Quantile")
-if(!empirical) colnames(Qp) <- c(paste0("RP.",RPs), "n")
-Qp
+plotci <- function(dist, qd=qdoy, RPs=c(20,50,100), ...)
+{
+RPs <- paste0("RP.", sort(RPs))
+ciBand(qd[dist,RPs[3],],qd[dist,RPs[1],],qd[dist,RPs[2],], ...)
+}
+
+
+# df <- names(sort(apply(qdoy[,"RMSE",], 1, mean, na.rm=TRUE), na.last=TRUE)) # quantile, probs=1:10/10
+
+pdf("qdoy.pdf", height=5)
+  par(mar=c(3,5,2,0.5), mgp=c(3.5,0.7,0))
+  plotqdoy("empirical")
+  plotqdoy("gev")
+  #
+  plotci("empirical", xaxt="n", xlab="", main="Q20, Q50 and Q100 in Cologne",
+         ylab="discharge  [m\U{00B3}/s]")
+  seasAxis(shift=117)
+  plotci("gev", add=TRUE, colm=4)
+  legend("topright", c("empirical", "gev"), fill=c(3,4))
+  # for(d in df[1:22]) plotqdoy(d)
+  # lines(1:366, qdoy["empirical","RP.100",], col=2)
+dev.off()
+
+# pdf("qdoy_30.pdf", height=5)
+# for(d in df[1:22]) plotqdoy(d, qdoy30)
+# #
+# plotci("empirical", qdoy30)
+# plotci("gev", qdoy30, add=TRUE, colm=4)
+# #
+# plotqdoy("gev", qdoy30)
+# plotqdoy("empirical", qdoy30)
+# plotqdoy("weighted2", qdoy30)
+# dev.off()
+
 }
--- R/qdoyPeriods.R
+++ R/qdoyPeriods.R
@@ -8,18 +8,20 @@
 #' @importFrom berryFunctions l2array
 #' @export
 #' @examples
+#' \dontrun{
 #' load(seasFolder("data/dismeta.Rdata"))
 #' qdoy_Koeln <- qdoyPeriods("Koeln")
 #' str(qdoy_Koeln)
 #' qdoy_Koeln <- list(Koeln=qdoy_Koeln, Mainz=qdoyPeriods("Mainz"))
 #' qdoyVis("Koeln", qdoy_Koeln)
 #' qdoyVis("Mainz", qdoy_Koeln)
+#' }
 #'
 #' @param name       Character: Name of gauge to be analyzed, see \code{\link{gnames}}
 #' @param disdf      Dataframe with columns "date" and \code{name}. DEFAULT: dis
 #' @param sans1999   Logical: exclude the values from 1999? (Pentecost flood)
 #'                   DEFAULT: FALSE
-#' @param probs,width,shift Arguments passed to \code{\link{qdoyCompute}}
+#' @param shift,RPs  Arguments passed to \code{\link{qdoyCompute}}
 #' @param time       Period beginnings (and ending of last one).
 #'                   DEFAULT: c(1920,1950,1980,2010)
 #' @param \dots      Further arguments passed to \code{\link{qdoyCompute}}
@@ -28,9 +30,8 @@ qdoyPeriods <- function(
 name,
 disdf=get("dis"),
 sans1999=FALSE,
-probs=c(0,0.5,0.9,0.99),
-width=7,
 shift=117,
+RPs=c(1,2,5,10,20,50,100,200),
 time=c(1920,1950,1980,2010),
 ...)
 {
@@ -42,7 +43,7 @@ if(sans1999) seldat[format(seldat$date,"%Y")=="1999",2] <- NA
 out <- lapply(seq_along(time[-1]), function(step){
   sdi <- selectDates(time[step], time[step+1], df=seldat)
   if(nrow(sdi)<1) sdi <- data.frame(date=Sys.Date()+1:10, emptystat=-1)
-  qdoyCompute(sdi$date, sdi[,2], quiet=TRUE, probs=probs, width=width, shift=shift, ...)
+  qdoyCompute(sdi$date, sdi[,2], quiet=TRUE, shift=shift, RPs=RPs, ...)
   })
 out <- l2array(out)
 names(dimnames(out))[3] <- "Period"
