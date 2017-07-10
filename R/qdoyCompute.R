8450f040bd371ec7f1a913b33551f190c0099ecb
brry <berryboessenkool@hotmail.com>
2017-07-10 16:56:22 +0200

    qdoyVis: initial release

--- R/qdoyCompute.R
+++ R/qdoyCompute.R
@@ -3,14 +3,14 @@
 #' \url{https://github.com/brry/berryFunctions/blob/master/R/seasonality.R}
 #' @return Matrix for each DOY and quantile
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2017
-#' @seealso \code{\link{qdoyPeriods}}
+#' @seealso \code{\link{qdoyVis}}, \code{\link{qdoyPeriods}}
 #' @keywords ts chron
 #' @importFrom berryFunctions l2array getColumn
 #' @importFrom pbapply pblapply
 #' @importFrom extremeStat distLextreme
 #' @export
 #' @examples
-#' # to be abstracted from testing
+#' # see qdoyVis
 #'
 #' @param dates    Dates in ascending order.
 #'                 Can be charater strings or \code{\link{strptime}} results,
@@ -56,61 +56,4 @@ names(dimnames(RL)) <- c("dist","rl","doy")
 dimnames(RL)[[2]] <- c(paste0("RP.", RPs), "RMSE")
 dimnames(RL)[[3]] <- 1:366
 RL
-}
-
-
-
-# Testing ----
-if(FALSE){
-
-
-load(seasFolder("data/dismeta.Rdata"))
-dd <- selectDates(1980,2020, df=dis)[,c("date","Koeln")]
-qdoy   <- qdoyCompute("date", "Koeln", data=dis, shift=117) # 10 secs
-qdoy30 <- qdoyCompute("date", "Koeln", data=dd,  shift=117) # 5 secs
-
-
-plotqdoy <- function(dist, qd=qdoy, RPs=c(1,2,10,50,200), cols=RPcols, ylim=lim0(10900)  )
-{
-plot(1:366, 1:366, ylim=ylim, las=1, type="n", main=dist, xaxt="n", xlab="",
-     ylab="discharge  [m\U{00B3}/s]")
-seasAxis(shift=117)
-RPs <- paste0("RP.",RPs)
-for(i in RPs) lines(1:366, qd[dist,i,], col=cols[i], lwd=2)
-textField(200, qd[dist,RPs,200], RPs, col=cols[RPs])
-}
-plotci <- function(dist, qd=qdoy, RPs=c(20,50,100), ...)
-{
-RPs <- paste0("RP.", sort(RPs))
-ciBand(qd[dist,RPs[3],],qd[dist,RPs[1],],qd[dist,RPs[2],], ...)
-}
-
-
-# df <- names(sort(apply(qdoy[,"RMSE",], 1, mean, na.rm=TRUE), na.last=TRUE)) # quantile, probs=1:10/10
-
-pdf("qdoy.pdf", height=5)
-  par(mar=c(3,5,2,0.5), mgp=c(3.5,0.7,0))
-  plotqdoy("empirical")
-  plotqdoy("gev")
-  #
-  plotci("empirical", xaxt="n", xlab="", main="Q20, Q50 and Q100 in Cologne",
-         ylab="discharge  [m\U{00B3}/s]")
-  seasAxis(shift=117)
-  plotci("gev", add=TRUE, colm=4)
-  legend("topright", c("empirical", "gev"), fill=c(3,4))
-  # for(d in df[1:22]) plotqdoy(d)
-  # lines(1:366, qdoy["empirical","RP.100",], col=2)
-dev.off()
-
-# pdf("qdoy_30.pdf", height=5)
-# for(d in df[1:22]) plotqdoy(d, qdoy30)
-# #
-# plotci("empirical", qdoy30)
-# plotci("gev", qdoy30, add=TRUE, colm=4)
-# #
-# plotqdoy("gev", qdoy30)
-# plotqdoy("empirical", qdoy30)
-# plotqdoy("weighted2", qdoy30)
-# dev.off()
-
 }
---
+++ R/qdoyVis.R
@@ -0,0 +1,64 @@
+#' @title Visualize seasonality of discharge
+#' @description  Visualize seasonality of discharge via quantile per DOY.
+#' @return Nothing, only plots
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jul 2017
+#' @seealso \code{\link{qdoyCompute}}, \code{\link{qdoyVisChange}}
+#' @keywords aplot
+#' @importFrom berryFunctions lim0
+#' @export
+#' @examples
+#' load(seasFolder("data/dismeta.Rdata"))
+#' dd <- selectDates(1990,2010, df=dis)[,c("date","Koeln")]
+#' qdoy <- qdoyCompute("date", "Koeln", data=dd, shift=117) # 2 secs
+#' str(qdoy)
+#' qdoyVis(qdoy, shift=117)
+#'
+#' qdoyVis(qdoy, main="Cologne 1990-2010", RPs=50, cols=4, ylim=c(2e3,10e3), shift=117, lab=0)
+#' qdoyVis(qdoy, dist="empirical", RPs=50, cols=3, add=TRUE, lab=0)
+#' legend("topright", c("empirical", "gev"), col=3:4, lwd=3)
+#'
+#' @param qd    Object returned by \code{\link{qdoyCompute}}
+#' @param dist  Distribution function: gev or empirical. DEFAULT: "gev"
+#' @param RPs   Return periods to be plotted (must be present in \code{qd}).
+#'              DEFAULT: c(1.111,2,10,50,200)
+#' @param cols  Colors for each line. Must have "RP.x" as names (or it is tried to set them).
+#'              DEFAULT: \code{\link{RPcols}}
+#' @param main  Plot title. DEFAULT: dist
+#' @param ylim  Y axis limits. DEFAULT: NA (computed internally from qd)
+#' @param add   Logical: add to existing plot?
+#' @param shift Shift of year break, see \code{\link{qdoyCompute}}. DEFAULT: 0
+#' @param labelindex Index along X axis at which to label lines.
+#'              Can be vectorized for each RP. 0 to suppress. DEFAULT: 200
+#' @param \dots Further arguments passed to \code{\link{plot}}
+#'
+qdoyVis <- function(
+qd,
+dist="gev",
+RPs=c(1.111,2,10,50,200),
+cols=RPcols,
+main=dist,
+ylim=NA,
+add=FALSE,
+shift=0,
+labelindex=200,
+...)
+{
+RPs <- paste0("RP.",RPs)
+if(is.na(ylim)) ylim <- lim0(qd[dist, RPs, ])
+if(!add)
+  {
+  plot(1:366, 1:366, ylim=ylim, las=1, type="n", main=main, xaxt="n", xaxs="i",
+       xlab="", ylab="discharge  [m\U{00B3}/s]", ...)
+  seasAxis(shift=shift)
+  }
+
+if(is.null(names(cols))) names(cols) <- RPs
+
+for(i in RPs) lines(1:366, qd[dist,i,], col=cols[i], lwd=2)
+labelindex <- rep(labelindex, length(RPs))
+names(labelindex) <- RPs
+if(!all(labelindex==0))
+for(i in RPs) textField(x=labelindex[i],
+                       y=mean(qd[dist,i,labelindex[i]+(-10:10) ]),
+                       labels=i, col=cols[i])
+}
