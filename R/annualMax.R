aa7a65593560d38512e4dc9b8714c89640755905
brry <berryboessenkool@hotmail.com>
2017-08-18 11:30:27 +0200

    annualMax: more columns returned

--- R/annualMax.R
+++ R/annualMax.R
@@ -1,18 +1,19 @@
-#' Annual maxima from time series
-#' 
-#' Get annual maxima from time series
-#' 
-#' @return data.frame with numerical columns "year" and "max". 
-#' The rownames are character strings with the year.
+#' @title Annual maxima from time series
+#' @description Get annual maxima and more information from time series
+#' @return data.frame with one row per (hydrological) year (h_year) with the columns:\cr
+#' year, max, date, doy (day of h_year), n (number of non-NA values), 
+#' days (number of days per h_year in input data) and rownr.\cr
+#' The rownames of the output are character strings with the h_year.
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2017
 #' @seealso \code{\link{qdoyCompute}}
 #' @importFrom berryFunctions getColumn
+#' @importFrom utils head
 #' @export
 #' @examples
 #' load(seasFolder("data/dismeta.Rdata"))
 #' annmax <- annualMax("date","Koeln", dis)
 #' head(annmax)
-#' plot(annmax, type="l", las=1, xlab="year")
+#' plot(annmax[,1:2], type="l", las=1, xlab="year")
 #' 
 #' head(annualMax("date","Koeln", dis))
 #' head(annualMax("date","Koeln", dis, shift=117)) # hydrological year
@@ -51,11 +52,22 @@ shift <- checkShift(shift)
 dates <- dates + shift
 
 # Annual maxima
-# axis values
 year <- as.numeric(format(dates,"%Y"))
-mymax <- function(xx) if(all(is.na(xx))) NA else max(xx, na.rm=TRUE)
-out <- tapply(X=values, INDEX=year, FUN=mymax)
-out <- data.frame(year=as.numeric(names(out)), max=out)
-out
+whichmax <- function(xx) if(all(is.na(xx))) NA else which.max(xx)
+m_which <- tapply(X=values, INDEX=year, FUN=whichmax)
+m_lengt <- tapply(X=values, INDEX=year, FUN=length)
+m_nonNA <- tapply(X=values, INDEX=year, FUN=function(x)sum(!is.na(x)) )
+m_index <- c(0,head(cumsum(m_lengt),-1)) + m_which
+
+# output data.frame
+out <- data.frame(year=year, max=values, date=dates-shift)
+out <- out[m_index,]
+out$year  <- as.numeric(names(m_which))
+out$doy   <- as.numeric(m_which)
+out$n     <- as.numeric(m_nonNA)
+out$days  <- as.numeric(m_lengt)
+out$rownr <- as.numeric(m_index)
+rownames(out) <- out$year
+return(out)
 }
 
