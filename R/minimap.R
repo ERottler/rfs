c2996c3b9016af4e73dfb3db8dfbd22bb78435df
brry <berryboessenkool@hotmail.com>
2017-09-07 16:06:09 +0200

    minimap: plotmap added

--- R/minimap.R
+++ R/minimap.R
@@ -1,5 +1,5 @@
 #' @title DEM plotting function
-#' @description Plots DEM as small inset map with desired stations highlighted
+#' @description Plots DEM as (small inset) map with desired stations highlighted
 #' @return None
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2017
 # @seealso \code{\link{help}}, \code{\link{help}}
@@ -9,12 +9,13 @@
 #' @importFrom graphics plot rasterImage points
 #' @export
 #' @examples
+#' plotmap("Koeln") ; axis(1) ; axis(2)
 #' plot(1:10)
 #' minimap("Koeln")
 #' minimap(0) # no stations
 #' minimap("Cochem", allargs=list(pch=3,cex=0.5))
 #' 
-#' stats <- gnames(app=TRUE)[c(1,5,8,12)]#' 
+#' stats <- gnames("app")[c(1,5,8,12)]
 #' minimap(stats, expr=text(meta[name,"lon"], meta[name,"lat"], name, col=2, adj=c(0,1)))
 #' 
 #' pdf("test.pdf", height=5)
@@ -31,13 +32,24 @@
 #' @param pch,lwd,col Point properties
 #' @param allargs     List of arguments passed to all gauge locations first
 #' @param expr        Expression to be executed after points, see example. DEFAULT: NULL
-#' @param \dots       Further arguments passed to \code{\link{points}}
+#' @param \dots       Further arguments passed to \code{plotmap} and then to \code{\link{points}}
 #' 
-minimap = function(
+minimap <- function(
  name,                # station name(s) to be plotted with red crosses
- metadf=meta,
  x1=0.1,x2=x1+0.2,    # relative location of minimap
  y1=0.7,y2=y1+0.27,
+ ...
+ )
+{
+smallPlot(plotmap(name=name, ...),
+x1=x1,x2=x2,y1=y1,y2=y2, mar=c(0,0,0,0), bg="transparent", border=NA)
+}
+
+#' @export
+#' @rdname minimap
+plotmap <- function(
+ name,                # station name(s) to be plotted with red crosses
+ metadf=meta,
  pch=3,               # point properties
  lwd=3,
  col="red",
@@ -49,7 +61,6 @@ minimap = function(
 dempath <- system.file("extdata/dem_small.png", package="rfs")
 checkFile(dempath)
 meta <- metadf
-smallPlot({
 plot(x=c(5.11, 12.02), y=c(46.24, 52.04), type="n", yaxs="i", xaxs="i", axes=F, asp=1.4, ann=F)
 rasterImage(png::readPNG(dempath),
                       xleft=5.11, ybottom=46.24-0.05,
@@ -57,7 +68,5 @@ rasterImage(png::readPNG(dempath),
 if(!is.null(allargs)) do.call(points, c(list(x=meta$lon, y=meta$lat), allargs))
 points(lat~lon, data=meta[name,], pch=pch, lwd=lwd, col=col, ...)
 eval(substitute(expr))
-},
-x1=x1,x2=x2,y1=y1,y2=y2, mar=c(0,0,0,0), bg="transparent", border=NA)
+invisible()
 }
-
